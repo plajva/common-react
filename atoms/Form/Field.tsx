4d0b95d72e2038883598d64f77686d1427f15246
Ruben Dario <rubendariopm14@gmail.com>
2021-07-05 20:30:57 -0400

    adding extra props to Field

--- atoms/Form/Field.tsx
+++ atoms/Form/Field.tsx
@@ -25,33 +25,37 @@ export type FieldProps = (
         // labelFirst?:boolean,
         labelPos?: 'top' | 'right' | 'bottom' | 'left';
         type?: 'checkbox' | 'email' | 'number' | 'text' | 'radio' | 'range' | 'select' | 'textarea';
+        hidden?: boolean;
+        // Item props
         rootProps?: React.LabelHTMLAttributes<HTMLElement>;
+        inputProps?: React.InputHTMLAttributes<HTMLElement>;
+        labelTextProps?: React.HtmlHTMLAttributes<HTMLElement>;
     };
 
 const Field: FunctionComponent<FieldProps> = (props) => {
-    let { className, as, rootProps, noField, name, label, labelPos, children, type, noLabelRoot, ...others } = props;
+    let { className, as, rootProps, inputProps, labelTextProps, noField, name, label, labelPos, children, type, noLabelRoot, style, hidden, ...others } =
+        props;
 
-    // Changing 'as' based on 'type'
+    // true when type is a 'select' or a 'textarea'
     let isNotInput = type && ['select', 'textarea'].includes(type);
+    // Make as = type if as not defined and type is a 'select' or 'textarea'
     if (!as && type && isNotInput) {
         as = type;
         type = undefined;
     }
-
+    
     // As defaults to 'input'
     as = as || 'input';
-
     // Switching to our custom fields
     switch (as) {
         case 'input':
             as = Input;
             break;
     }
-
+    
     // Defining position
     const radio_or_check = ['radio', 'checkbox'].includes(type);
     labelPos = labelPos || (radio_or_check && 'right') || 'top';
-
     const t_or_b = ['top', 'bottom'].includes(labelPos);
 
     // Defining field
@@ -60,21 +64,25 @@ const Field: FunctionComponent<FieldProps> = (props) => {
         fieldChildren = children;
         children = undefined;
     }
-    let formik = useFormikContext();
+    
     const field = noField ? (
         as
     ) : (
         <FField
-            style={
-                t_or_b
+            style={{
+                ...(hidden ? {opacity: 0, width: 0, height: 0} :
+                    (t_or_b
                     ? { marginLeft: 'auto', marginRight: 'auto', display: 'block' }
-                    : { width: radio_or_check ? 30 : '50%', height: radio_or_check ? 'auto' : undefined }
-            }
-            className={classNameFind(s, `field`)}
+                    : { width: radio_or_check ? 30 : '50%', height: radio_or_check ? 'auto' : undefined })),
+                ...style,
+                ...(inputProps?.style || {})
+            }}
+            className={classNameFind(s, `field`, inputProps?.className)}
             name={name}
             as={as}
             type={type}
             {...others}
+            {...(inputProps||{})}
             children={fieldChildren}
         />
     );
@@ -82,8 +90,8 @@ const Field: FunctionComponent<FieldProps> = (props) => {
     // Defining label
     const labelText = (label || children) && (
         <div
-            className={classNameFind(s, `label`, `label-${labelPos}`)}
-            style={t_or_b ? {} : { width: radio_or_check ? 'calc(100% - 40px)' : '50%', float: labelPos }}
+            className={classNameFind(s, `label`, `label-${labelPos}`, labelTextProps?.className)}
+            style={hidden ? {width:'100%'} : (t_or_b ? {} : { width: radio_or_check ? 'calc(100% - 40px)' : '50%', float: labelPos })}
         >
             <div style={children ? { marginBottom: '10px' } : {}}>{label}</div>
             {children}
@@ -93,8 +101,8 @@ const Field: FunctionComponent<FieldProps> = (props) => {
     return React.createElement(
         noLabelRoot ? 'div' : 'label',
         {
-            className: classNameFind(s, `atom`, radio_or_check ? 'radio' : '', 'dup', className),
-            ...rootProps,
+            ...(rootProps || {}),
+            className: classNameFind(s, `atom`, radio_or_check ? 'radio' : '', 'dup', className, rootProps?.className),
         },
         <>
             {labelPos == 'right' || labelPos == 'bottom' ? (

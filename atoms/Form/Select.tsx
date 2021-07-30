377bc9d3a5a78f5bd34e4b74b621c42d2d8d30b6
Ernesto J. Romero <ejromero1990@gmail.com>
2021-07-30 19:06:50 -0400

    Add placeholder props to Select field.

--- atoms/Form/Select.tsx
+++ atoms/Form/Select.tsx
@@ -4,15 +4,22 @@ import React, { forwardRef, ReactNode } from 'react';
 import { useFormField, UseFormFieldProps } from './Form';
 import s from './Select.module.scss';
 
+interface PlaceholderProps {
+    disabled?: boolean;
+    value?: string | number;
+}
+
 export interface SelectProps extends UseFormFieldProps {
     children?: ReactNode;
+    placeholderProps?: PlaceholderProps;
 }
 type SelectPropsF = SelectProps & React.SelectHTMLAttributes<HTMLElement>;
-const Select = ({ className, children, ..._props }: SelectPropsF, ref) => {
+const Select = ({ className, placeholder, placeholderProps, children, ..._props }: SelectPropsF, ref) => {
     const theme = useTheme().name;
     className = classFind(s, `comp`, className, 'dup', theme);
 
     const { value, ...props } = useFormField(_props);
+    const placeProps = { disabled: !!placeholderProps?.disabled, selected: true };
     return (
         <select
             data-value={value || ''}
@@ -21,7 +28,9 @@ const Select = ({ className, children, ..._props }: SelectPropsF, ref) => {
             {...props}
             ref={typeof ref === 'object' && !Object.keys(ref).length ? null : ref}
         >
-            <option></option>
+            <option {...placeProps} value={placeholderProps?.value || ''}>
+                {placeholder}
+            </option>
             {/* Making sure selected element is the one with selected attribute */}
             {/* {Children.map(
 				children,

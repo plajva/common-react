65ec449ca3edb91ad09ede71e1c186aee951c5d4
Ernesto J. Romero <ejromero1990@gmail.com>
2021-08-06 00:53:43 -0400

    Update css of InputDate. Update color of Inputs placeholders.

--- atoms/Form/InputDate.tsx
+++ atoms/Form/InputDate.tsx
@@ -5,6 +5,7 @@ import MomentLocaleUtils from 'react-day-picker/moment';
 import { useTheme } from '../Theme';
 import { classNameFind } from '../../utils';
 
+import 'react-day-picker/lib/style.css';
 import s from './InputDate.module.scss';
 
 interface InputDateProps {
@@ -35,6 +36,7 @@ const InputDate = ({
         <ReactDay
             ref={ref}
             value={value}
+            showOverlay={true}
             placeholder={placeholder}
             inputProps={{
                 className: classNameFind(s, 'atom', theme) + ' ' + className,

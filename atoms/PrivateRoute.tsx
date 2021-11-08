c1fe02139771d83b12b363e88c65747b3d5a7d2a
Ruben Dario <rubendariopm14@gmail.com>
2021-11-08 16:23:33 -0500

    making Private Route

--- atoms/PrivateRoute.tsx
+++ atoms/PrivateRoute.tsx
@@ -3,29 +3,70 @@
  * Updated private route, previous private route wasn't working with 'component' prop
  */
 import React from "react";
-import { Route, Navigate, IndexRouteProps, LayoutRouteProps, PathRouteProps } from "react-router-dom";
+import { Route, Navigate, IndexRouteProps, LayoutRouteProps, PathRouteProps, Outlet } from "react-router-dom";
 
 export interface PrivateRouteProps {
 	assert: () => boolean;
 	onAssertFalse?: string | (React.ReactElement | null);
 }
-const PrivateRoute = ({ element, assert, onAssertFalse, ...rest }: PrivateRouteProps & (PathRouteProps | LayoutRouteProps | IndexRouteProps)) => {
-	const children = assert() ? (
-		element
-	) : typeof onAssertFalse === "string" || !onAssertFalse ? (
+/**
+ * ! Depracated, use PrivateOutlet instead like this:
+ * 			<Route path="/private-outlet" element={<PrivateOutlet />}>
+          <Route element={<Private />} />
+        </Route>
+ * @param param0 
+ * @returns 
+ */
+// const PrivateRoute = ({ element, assert, onAssertFalse, ...rest }: PrivateRouteProps & (PathRouteProps | LayoutRouteProps | IndexRouteProps)) => {
+// 	const children = assert() ? (
+// 		element
+// 	) : typeof onAssertFalse === "string" || !onAssertFalse ? (
+// 		<Navigate to={onAssertFalse ?? "/login"} />
+// 	) : (
+// 		onAssertFalse
+// 	)
+	
+// 	return (
+// 		// Show the component or children only when assert is true
+// 		// Otherwise, redirect the user to /signin page
+// 		<Route
+// 			{...rest}
+// 			element={children}
+// 		/>
+// 	);
+// };
+// export const PrivateOutlet = ({assert, onAssertFalse, children} : PrivateRouteProps & {children?}) => {
+// 	return assert() ? (
+// 		<Outlet /> ?? children
+// 	) : typeof onAssertFalse === "string" || !onAssertFalse ? (
+// 		<Navigate to={onAssertFalse ?? "/login"} />
+// 	) : (
+// 		onAssertFalse
+// 	);
+// }
+
+type PrivateProps = PrivateRouteProps & (PathRouteProps | LayoutRouteProps | IndexRouteProps);
+
+const PrivateElement: React.FC<PrivateProps> = ({ assert, onAssertFalse, element }) => {
+  const valid = assert();
+
+  if (valid) {
+    return element ?? null
+  }
+
+  return typeof onAssertFalse === "string" || !onAssertFalse ? (
 		<Navigate to={onAssertFalse ?? "/login"} />
 	) : (
-		onAssertFalse
+		onAssertFalse ?? null
 	)
-	
-	return (
-		// Show the component or children only when assert is true
-		// Otherwise, redirect the user to /signin page
-		<Route
-			{...rest}
-			element={children}
-		/>
-	);
-};
+}
+
+export const PrivateRoute: React.FC<PrivateProps> = ({ element, assert, onAssertFalse, ...rest }) => {
+  return <Route {...rest} element={<PrivateElement element={element} assert={assert} onAssertFalse={onAssertFalse}  />} />
+}
+
+// export const PrivateOutletHOC = () => {
+// 	return 
+// }
 
 export default PrivateRoute;

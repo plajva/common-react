e12d8c93ffddb1838703239d240fdaa71c065fa4
Ruben Dario <rubendariopm14@gmail.com>
2022-09-20 09:29:35 -0400

    updating Locked

--- atoms/Locked.ts
+++ atoms/Locked.ts
@@ -6,27 +6,24 @@ export interface LockedProps {
 	assertRoles?: string;
 }
 
+const toAssertion = (rule: string | undefined, array: string[] | undefined) =>
+	rule
+		? rule
+				.split(' ')
+				.map((v) =>
+					v
+						.split('|')
+						.map((x) => x.trim().toLowerCase())
+						.filter((x) => !!x)
+				)
+				.every((c) => c.some((c) => array?.includes(c)))
+		: true;
 /**
  * Locked if user logged out, or user/claims doesn't contain assertClaims/assertRoles passed
  * @returns boolean
  */
 export const isLocked = ({ assertClaims, assertRoles }: LockedProps) =>
-	!(
-		(assertClaims
-			? assertClaims
-					.split(' ')
-					.map((x) => x.trim().toLowerCase())
-					.filter((x) => !!x)
-					.every((c) => token.claims?.includes(c))
-			: true) &&
-		(assertRoles
-			? assertRoles
-					.split(' ')
-					.map((x) => x.trim().toLowerCase())
-					.filter((x) => !!x)
-					.every((c) => token.roles?.includes(c))
-			: true)
-	) || !isLoggedIn();
+	!(toAssertion(assertClaims, token.claims) && toAssertion(assertRoles, token.roles)) || !isLoggedIn();
 
 /**
  * Same as: isLocked({assertClaims,assertRoles}) ? null : (children ?? null)
